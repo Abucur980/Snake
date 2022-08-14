82a9363778d1fdf7d32ac750172b72969b39ea5b
Andrei Bucur <abucur980@gmail.com>
2022-08-14 17:15:03 +0300

    add conditions for game over

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     // generate board grid 20x20
     for (let i = 0; i < 400; ++i) {
-        document.getElementById('game-board').insertAdjacentHTML('beforeend', '<div class="board-cell"></div>');
+        document.getElementById('game-board').insertAdjacentHTML('beforeend', '<div class="board-cell">'+ i +'</div>');
     }
 
     let movement = 1;
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function startMovement() {
         // define the snake
-        snake = [81, 82, 83];
+        snake = [83, 82, 81];
         snake.forEach(index => cells[index].classList.add('snake'));
         // call the function movementResults once in 250 milliseconds
         interval = setInterval(movementResults, 250);
@@ -32,8 +32,26 @@ document.addEventListener('DOMContentLoaded', () => {
             movement = -1;
         }
     }
-
+    console.log(339 % 20);
     function movementResults() {
+        // game over
+        if (
+            // movement is used in order to have the edgy moves
+            // if the snake hits the top edge
+            (snake[0] - 20 < 0 && movement === -20) ||
+            // if the snake hits the right edge
+            (snake[0] % 20 === 19 && movement === 1) ||
+            // if the snake hits the bottom edge
+            (snake[0] + 20 >= 400 && movement === 20) ||
+            // if the snake hits the left edge
+            (snake[0] % 20 === 0 && movement === -1) ||
+            // if the snake tries to go backwards or tries to go into itself
+            cells[snake[0] + movement].classList.contains("snake")
+        ) {
+            // if any of the conditions is met, stop the movement
+            return clearInterval(interval);
+        }
+
         const lastSnakeCell = snake.pop();
         // remove snake class from lastSnakeCell in order to create the effect of snake moving
         cells[lastSnakeCell].classList.remove('snake');
